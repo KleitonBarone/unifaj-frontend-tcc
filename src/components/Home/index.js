9c12c3d36f9d72cec9238b8ccd284bb027a9e552
KleitonBarone <kleitonpolar6@gmail.com>
2021-08-05 23:07:17 -0300

    Configuração inicial

---
+++ src/components/Home/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+    Container,
+    Text
+} from './styles.js';
+
+function Home() {
+  return (
+    <Container>
+      <Text>
+        <h1>Front End TCC</h1>
+        <h2>
+          Unifaj
+        </h2>
+      </Text>
+    </Container>
+  );
+}
+
+export default Home;
