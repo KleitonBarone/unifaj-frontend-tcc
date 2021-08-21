a08b69779d2274665725fa4850a99ff85d19badc
LeonardoMonsuete <leonardo.monsuete@outlook.com>
2021-08-21 17:58:55 -0300

    "Componentes criados SignIn, Up e dashboard M-UI"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import Home from "./components/Home";
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 function App() {
-  return <Home />;
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Home />
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Home from "./components/Home";
 import Profile from "./components/Perfil";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import Dashboard from "./components/Dashboard";
 
 function Routes() {
   return (
@@ -10,6 +13,10 @@ function Routes() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/profile" component={Profile} />
+        <Route path="/login" component={SignIn} />
+        <Route path="/inscrever" component={SignUp} />
+        <Route path="/dashboard" component={Dashboard} />
+        
       </Switch>
     </BrowserRouter>
   );
