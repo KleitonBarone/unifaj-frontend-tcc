2b5125d687220129ef052642ad69f066e75d410f
LeonardoMonsuete <leonardo.monsuete@outlook.com>
2021-10-12 12:01:39 -0300

    Adicionado transition fade nos slides do carousel, adicionado tambem autoplay e indice de slide

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -4,14 +4,43 @@ import { images } from "components/Helpers/CarouselData";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 
+const delay = 3500;
+
 function Carousel() {
   const [currImg, setCurrImg] = useState(0);
+
+  // const [index, setCurrImg] = React.useState(0);
+  const timeoutRef = React.useRef(null);
+
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
+  React.useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setCurrImg((prevIndex) =>
+          prevIndex === images.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [currImg]);
+
+
 
   return (
     <div className="carousel">
       <div
         className="carouselInner"
         style={{ backgroundImage: `url(${images[currImg].img})` }}
+ 
       >
         <div
           className="left"
@@ -20,11 +49,23 @@ function Carousel() {
             currImg == 0 && setCurrImg(images.length - 1);
           }}
         >
-          <ArrowBackIosIcon style={{ fontSize: 18 }} />
+          <ArrowBackIosIcon style={{ fontSize: 22 }} />
         </div>
-        <div className="center">
-          <h1>{images[currImg].title}</h1>
-          <p>{images[currImg].subtitle}</p>
+        <div className="center CarouselText">
+          <h1 className="CarouselText">{images[currImg].title}</h1>
+          <p className="CarouselText">{images[currImg].subtitle}</p>
+
+          <div className="slideshowDots">
+        {images.map((_, idx) => (
+          <div
+            key={idx}
+            className={`slideshowDot${currImg === idx ? " active" : ""}`}
+            onClick={() => {
+              setCurrImg(idx);
+            }}
+          ></div>
+        ))}
+      </div>
         </div>
         <div
           className="right"
@@ -33,8 +74,11 @@ function Carousel() {
             currImg == images.length - 1 && setCurrImg(0);
           }}
         >
-          <ArrowForwardIosIcon style={{ fontSize: 18 }} />
+          <ArrowForwardIosIcon style={{ fontSize: 22 }} />
         </div>
+        
+      
+
       </div>
     </div>
   );
